a18a824fd453bc473fb9b9352977764e634210c5
Fabiano Avelar <fabianoavelar86@gmail.com>
2024-05-01 12:48:46 -0300

    add healthcheck e swagger

--- src/main/java/com/itau/desafioprogramacao/DesafioProgramacaoApplication.java
+++ src/main/java/com/itau/desafioprogramacao/DesafioProgramacaoApplication.java
@@ -1,9 +1,15 @@
 package com.itau.desafioprogramacao;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "desafio-itau-vaga-99-junior",
+								version = "1",
+								description = "API desenvolvida para o Desafio de Programação Itaú"))
+
 public class DesafioProgramacaoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/itau/desafioprogramacao/controller/TransacaoController.java
+++ src/main/java/com/itau/desafioprogramacao/controller/TransacaoController.java
@@ -3,32 +3,52 @@ package com.itau.desafioprogramacao.controller;
 import com.itau.desafioprogramacao.dto.EstatísticasDTO;
 import com.itau.desafioprogramacao.dto.TransacaoDTO;
 import com.itau.desafioprogramacao.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/transacao")
+@RequestMapping(value = "/transacao", produces = {"application/json"})
 public class TransacaoController {
 
     @Autowired
     private TransacaoService transacaoService;
 
+    @Operation(summary = "Cria uma transação", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação criada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível"),
+    })
     @PostMapping
     public ResponseEntity<Void> salvar(@RequestBody @Valid TransacaoDTO transacaoDTO) {
         transacaoService.salvar(transacaoDTO);
         return ResponseEntity.status(201).build();
     }
 
+    @Operation(summary = "deleta todas as transações", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível"),
+    })
     @DeleteMapping
     public ResponseEntity<Void> deletar() {
         transacaoService.deletar();
         return ResponseEntity.status(200).build();
     }
 
+    @Operation(summary = "Mostra as estatísticas das transações cadastradas", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível"),
+    })
     @GetMapping
     public ResponseEntity<EstatísticasDTO> findAll() {
+
         return ResponseEntity.ok(transacaoService.estatisticas());
     }
 }
